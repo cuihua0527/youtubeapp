5e768f935eb7c80c5c457b25e1c6eb81926b7f03
최화 <cuihua0527@gmail.com>
2026-04-03 22:28:10 +0900

    Initial Spring Boot login page

---
+++ src/main/java/com/youtubuapp/web/LoginController.java
@@ -0,0 +1,20 @@
+package com.youtubuapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("pageTitle", "Youtubu 로그인");
+        return "login";
+    }
+}
